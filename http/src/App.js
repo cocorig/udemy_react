45871f5982f160b5e28af70e718d622c1d34729f
Hayeon <fhdodl@naver.com>
2023-10-08 22:18:46 +0900

    로딩 조건부 렌더링 추가

--- http/src/App.js
+++ http/src/App.js
@@ -1,52 +1,45 @@
-import React, {useState} from 'react';
+import React, {useState , useEffect} from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movie , setMovie] = useState([])
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
-  // then-> async , await (단순 코드 변환, 똑깉은 작업, 앞에서 미리 선언, 가독성)
-async function fetchMoviesHandler(){
+  const [isLoading , setIsLoading] = useState(false);
 
+async function fetchMoviesHandler(){
+  setIsLoading(true); // 가져오기 기다리는 중.. 로딩 중~
   const response = await fetch('https://swapi.dev/api/films/');
   const data = await response.json();
-  // js객체로 전달된 데이터를 반환하기 전에 props로 넘겨줄 키 이름을 바꿔줌
   const transformMovies =  data.results.map((items)=> {
-  return { // 한 줄 한줄 객체를 리턴함
+  return {
     id : items.episode_id,
     title : items.title,
     openingText: items.opening_crawl,
     releaseDate: items.release_date,
       };
     })
-    setMovie(transformMovies) // 새로운 data 배열
-  }
-
+    setMovie(transformMovies) 
+    setIsLoading(false); //로딩 중 아님~ 다 가져옴
+  } 
+console.log(isLoading);
   return (
     <React.Fragment>
       <section>
-        {/* 버튼이 클릭될 때마다 영화 정보를 가져옴 */}
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section> 
-        {/* 그 결과를 화면에 보여줌 */}
-        <MoviesList movies={movie} />
+      <section>
+        {/* 로딩 중이 아닐 때(isLoading이 false일 때, ! not연산자로 true가 되니까 뒤에 컴포넌트가 렌더링) MoviesList가 보여지게 하자 , 다 가져오면 렌더링 되게 */}
+        {/* 받아온 데이터가 1개라도 있을 때 */}
+        {!isLoading && movie.length > 0 &&  <MoviesList movies={movie}/>}
+        {/* 가져왔는데 영화가 없다면 */}
+        {!isLoading && movie.length === 0 && <strong>Found no movies.</strong>}
+        {isLoading && <strong>Loading...</strong>}
       </section>
+      
     </React.Fragment>
   );
 }
 
 export default App;
+
+// 앞에께 false면 뒤에 실행
